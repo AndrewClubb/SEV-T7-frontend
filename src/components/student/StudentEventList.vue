c66478e94504de9305a8732a1afc630b4c3a5f76
Alexandria Adams <alexm.adams@yahoo.com>
2023-04-15 20:02:19 -0500

    Getting actual data into event sign up

--- src/components/student/StudentEventList.vue
+++ src/components/student/StudentEventList.vue
@@ -28,7 +28,7 @@
                   <v-btn
                     small
                     color="primary"
-                    @click="changeCurrentEvent(item.raw)"
+                    @click="makeCurrentEvent(item.raw)"
                     >Sign Up Here</v-btn
                   >
                 </div>
@@ -70,14 +70,6 @@
         </v-row> -->
         <v-row>
           <v-col cols="3">
-            <v-select v-model="instAvail" label="Instruc Availability">
-            </v-select>
-            <!-- <v-btn variant="text" rounded> clear filters </v-btn> -->
-          </v-col>
-          <v-col cols="3">
-            <v-select v-model="accAvail" label="Accomp Availability"></v-select>
-          </v-col>
-          <v-col cols="3">
             <v-select
               clearable
               v-model="selectedStudentInstrument"
@@ -87,6 +79,38 @@
               return-object
             ></v-select>
           </v-col>
+          <v-col cols="3">
+            <v-text-field
+              :model-value="
+                selectedStudentInstrument != null
+                  ? selectedStudentInstrument.instructor.user.fName +
+                    ' ' +
+                    selectedStudentInstrument.instructor.user.lName
+                  : ''
+              "
+              class="mb-2"
+              label="Instructor"
+              readonly
+              @change="showAvailability()"
+            >
+            </v-text-field>
+          </v-col>
+          <v-col cols="3">
+            <v-text-field
+              :model-value="
+                selectedStudentInstrument != null
+                  ? selectedStudentInstrument.accompanist.user.fName +
+                    ' ' +
+                    selectedStudentInstrument.accompanist.user.lName
+                  : ''
+              "
+              class="mb-2"
+              label="Accompanist"
+              readonly
+              @change="showAvailability()"
+            >
+            </v-text-field>
+          </v-col>
         </v-row>
         <v-row>
           <v-col>
@@ -94,71 +118,23 @@
               <v-sheet max-height="400" class="overflow-y-auto" color="fav">
                 <v-row>
                   <v-col
-                    v-for="event in events"
-                    :key="event.id"
-                    v-on:click="changeCurrentEvent(event)"
-                    variant="tonal"
-                    v-bind:color="
-                      event.id === currentEvent.id ? 'blue' : 'cyan-darken-2'
-                    "
+                    v-for="(time, index) in currentEventTimes"
+                    :key="index"
                     cols="6"
+                    class="pa-1 ma-0 d-flex justify-center"
                   >
-                    <v-btn size="x-large" elevation="5" class="p-3 ma-2">
-                      12:00 - 12:05
-                    </v-btn>
-                    <!-- <v-btn size="x-large" elevation="5" class="p-3 ma-2">
-                      12:00 - 12:05
-                    </v-btn>
-                    <v-btn size="x-large" elevation="5" class="p-3 ma-2">
-                      12:10 - 12:15
-                    </v-btn>
                     <v-btn
-                      size="x-large"
-                      elevation="5"
-                      class="p-3 ma-2"
+                      :style="{ width: '180px' }"
+                      size="large"
+                      elevation="6"
+                      class="pa-0 pl-auto ma-1 mt-3"
                     >
-                      12:20 - 12:25
+                      {{
+                        this.formatTime(time.startTime) +
+                        " - " +
+                        this.formatTime(time.endTime)
+                      }}
                     </v-btn>
-                    <v-btn size="x-large" elevation="5" class="p-3 ma-2">
-                      12:30 - 12:35
-                    </v-btn>
-                    <v-btn size="x-large" elevation="5" class="p-3 ma-2">
-                      12:40 - 12:45
-                    </v-btn>
-                    <v-btn size="x-large" elevation="5" class="p-3 ma-2">
-                      12:50 - 12:55
-                    </v-btn>
-                    <v-btn size="x-large" elevation="5" class="p-3 ma-2">
-                      01:05 - 01:10
-                    </v-btn>
-                  </v-col>
-                  <v-col cols="6">
-                    <v-btn size="x-large" elevation="5" class="p-3 ma-2">
-                      12:05 - 12:10
-                    </v-btn>
-                    <v-btn size="x-large" elevation="5" class="p-3 ma-2">
-                      12:15 - 12:20
-                    </v-btn>
-                    <v-btn
-                      size="x-large"
-                      elevation="5"
-                      color="cyan-darken-2"
-                      class="p-3 ma-2"
-                    >
-                      12:25 - 12:30
-                    </v-btn>
-                    <v-btn size="x-large" elevation="5" class="p-3 ma-2">
-                      12:35 - 12:40
-                    </v-btn>
-                    <v-btn size="x-large" elevation="5" class="p-3 ma-2">
-                      12:45 - 12:50
-                    </v-btn>
-                    <v-btn size="x-large" elevation="5" class="p-3 ma-2">
-                      12:55 - 01:00
-                    </v-btn>
-                    <v-btn size="x-large" elevation="5" class="p-3 ma-2">
-                      01:10 - 01:15
-                    </v-btn> -->
                   </v-col>
                 </v-row>
               </v-sheet>
@@ -190,12 +166,12 @@
                   :style="{ width: '175px' }"
                 ></v-autocomplete>
               </v-col>
-              <!-- <v-col cols="6">
+              <v-col v-if="selectedEvent.type === 'Jury'" cols="6">
                 <v-btn class="pa-1 ma-3"> Add Piece </v-btn>
                 <v-sheet class="overflow-y-auto" color="fav">
                   <v-list lines="two"> </v-list>
                 </v-sheet>
-              </v-col> -->
+              </v-col>
             </v-row>
             <v-row>
               <input class="pa-1 ma-1" type="checkbox" v-model="transBool" />
@@ -235,178 +211,23 @@
                 >Close</v-btn
               >
               <v-spacer></v-spacer>
+              <!-- @click="signUp" below -->
               <v-btn color="blue-darken-1"> save </v-btn>
             </v-card-actions>
           </v-col>
         </v-row>
-        <!-- <v-data-table
-          class="mt-15 mb-5 elevation-1"
-          :items="currentAvailability"
-          :headers="availabilityHeader"
-          :sort-by="[{ key: 'startTime', order: 'dsc' }]"
-        >
-          <template #top>
-            <v-toolbar flat>
-              <v-toolbar-title> AVAILABILITY </v-toolbar-title>
-              <div v-if="currentAvailability.length == 0" class="mr-4">
-                <v-spacer></v-spacer>
-                You currently have no availability for this event.
-              </div>
-            </v-toolbar>
-          </template>
-          <template #item="{ item }">
-            <tr>
-              <td v-for="(header, index) in availabilityHeader" :key="index">
-                <div
-                  v-if="
-                    header.title == 'Start Time' || header.title == 'End Time'
-                  "
-                >
-                  {{ this.formatTime(item.columns[header.key]) }}
-                </div>
-                <div v-else>
-                  <v-icon size="small" class="me-2" @click="editItem(item.raw)">
-                    mdi-pencil
-                  </v-icon>
-                  <v-icon size="small" @click="deleteItem(item.raw)">
-                    mdi-delete
-                  </v-icon>
-                </div>
-              </td>
-            </tr>
-          </template>
-        </v-data-table> -->
       </v-card-text>
-      <!-- <v-card-actions>
-        <v-btn color="blue-darken-1" variant="text" @click="showDialog = false"
-          >Close</v-btn
-        >
-        <v-btn color="blue-darken-1" class="pa-1 ma-1"> save </v-btn>
-      </v-card-actions> -->
     </v-card>
-    <!-- DELETE dialog button -->
-    <v-dialog v-model="dialogDelete" max-width="500px">
-      <v-card>
-        <v-card-title class="text-h5"
-          >Are you sure you want to delete this item?</v-card-title
-        >
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue-darken-1" variant="text" @click="closeDelete"
-            >Cancel</v-btn
-          >
-          <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm"
-            >OK</v-btn
-          >
-          <v-spacer></v-spacer>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <v-dialog v-model="dialogEdit" max-width="500px">
-      <v-card>
-        <v-card-text>
-          <v-row class="ml-5">
-            <strong class="text-red-lighten-1">{{
-              this.editErrorMessage
-            }}</strong>
-          </v-row>
-          <v-row class="mt-4 ml-5">
-            <v-col cols="6"
-              >{{ "Start Time-" + this.editStartOriginal }}
-            </v-col>
-            <v-col cols="6">
-              {{ "End Time-" + this.editEndOriginal }}
-            </v-col>
-          </v-row>
-          <v-row class="mt-4 ml-5">
-            <v-col cols="6">
-              <v-select
-                v-model="editSelectedStart"
-                label="Start Time"
-                :items="availabilityStartArray"
-                return-object
-                :style="{ width: '160px' }"
-                @update:modelValue="editStartChange()"
-              ></v-select>
-            </v-col>
-            <v-col cols="6">
-              <v-select
-                class="mr-15"
-                v-model="editSelectedEnd"
-                label="End Time"
-                :items="editEndArray"
-                return-object
-                :style="{ width: '160px' }"
-              ></v-select>
-            </v-col>
-          </v-row>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue-darken-1" variant="text" @click="closeEdit"
-            >Cancel</v-btn
-          >
-          <v-btn color="blue-darken-1" variant="text" @click="editItemConfirm"
-            >SAVE</v-btn
-          >
-          <v-spacer></v-spacer>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </v-dialog>
-
-  <!-- <div>
-    <v-container>
-      <v-row>
-        <v-col>
-          <h3>Events</h3>
-          <br />
-          <v-card height="500" width="300" class="scrollable">
-            <v-btn
-              v-for="event in events"
-              :key="event.id"
-              v-on:click="changeCurrentEvent(event)"
-              variant="tonal"
-              v-bind:color="event.id === currentEvent.id ? 'blue' : 'black'"
-            >
-              {{ event.type + " - " + event.date }}
-            </v-btn>
-          </v-card>
-
-          <br />
-        </v-col>
-        <v-col>
-          <h3>Avaliable Times</h3>
-          <br />
-          <v-card height="500" class="scrollable">
-            <v-checkbox
-              v-for="(time, index) in currentEventTimes"
-              :key="index"
-              v-model="selectedEventTimes"
-              :label="time.startTime"
-              :value="time"
-            >
-            </v-checkbox>
-          </v-card>
-          <br />
-          <v-btn>Next</v-btn>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div> -->
 </template>
 
 <script>
-// import EventTimeDataService from "../../services/EventTimeDataService";
 import Utils from "../../config/utils.js";
 import { reactive } from "vue";
 import AvailabilityDataService from "../../services/AvailabilityDataService";
 import ComposerDataService from "../../services/ComposerDataService";
 import EventDataService from "../../services/EventDataService";
+import EventTimeDataService from "../../services/EventTimeDataService";
 import SongDataService from "../../services/SongDataService";
 import StudentInstrumentDataService from "../../services/StudentInstrumentDataService";
 import StudentRepertoire from "./StudentRepertoire.vue";
@@ -429,7 +250,6 @@ export default {
     userChosenSlot: [],
     selectedEvent: null,
     checked: false,
-    //Inside dialogue
     eventHeader: [
       { title: "Start Time", key: "startTime", sortable: false },
       { title: "End Time", key: "endTime", sortable: false },
@@ -445,17 +265,60 @@ export default {
     selectedStudentInstrument: null,
     composerSearch: null,
     transBool: false,
-    //maybe
+    instAvail: [],
+    accAvail: [],
     selectedEvent: null,
-    currentEvent: {},
     currentEventTimes: [],
     selectedEventTimes: [],
-    // currentDate: new Date(),
+    isValid: true,
   }),
   methods: {
-    changeCurrentEvent(event) {
-      this.currentEvent = event;
-      this.determineEventTimes();
+    // async signUp() {
+    //   if (!this.isValid()) {
+    //     return;
+    //   }
+
+    //   const data = {
+    //     studentInstrumentId: this.selectedStudentInstrument.id,
+    //     songId: this.selectedSong.id,
+    //     semesterId:
+    //       this.selectedSemester == null ? null : this.selectedSemester.id,
+    //   };
+
+    //   await RepertoireDataService.create(data).catch((e) => {
+    //     console.log(e);
+    //   });
+
+    //   this.fillSemesters();
+    //   this.fillRepertoire();
+    //   this.closeDialog();
+    // },
+    async showAvailability() {
+      this.selectedStudentInstrument.instructor.user.availabilities =
+        this.instAvail;
+      this.selectedStudentInstrument.accompanist.user.availabilities =
+        this.accAvail;
+
+      console.log(instAvail);
+      console.log(accAvail);
+    },
+    async makeCurrentEvent(event) {
+      this.errorMessage = "";
+      this.selectedEvent = event;
+      this.currentSlot = this.userChosenSlot.filter(
+        (obj) => obj.eventId == event.id
+      );
+
+      await EventTimeDataService.getByEvent(event.id)
+        .then((response) => {
+          this.currentEventTimes = response.data;
+          // console.log(this.currentEventTimes);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+
+      this.showDialog = true;
     },
     async retrieveEventsDateAndAfter(date) {
       await EventDataService.getGTEDate(date)
@@ -466,6 +329,20 @@ export default {
         .catch((e) => {
           console.log(e);
         });
+    },
+    async retrieveStudentInstruments() {
+      await StudentInstrumentDataService.getByUser(this.user.userId)
+        .then((response) => {
+          this.studentInstruments = response.data;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+      console.log(this.studentInstruments);
+      console.log(this.selectedStudentInstrument);
+    },
+    handleClick() {
+      this.active = !this.active;
     },
     async composerUpdated() {
       this.selectedSong = null;
@@ -479,18 +356,6 @@ export default {
             console.log(e);
           });
       }
-    },
-    async retrieveStudentInstruments() {
-      await StudentInstrumentDataService.getByUser(this.user.userId)
-        .then((response) => {
-          this.studentInstruments = response.data;
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    },
-    handleClick() {
-      this.active = !this.active;
     },
     fillComposers() {
       ComposerDataService.getAll()
@@ -511,50 +376,6 @@ export default {
     },
     stuRep() {
       this.$router.push({ path: "studentRepertoire" });
-      // this.$route.fullPath === "/studentRepertoire";
-    },
-    //change these accordingly
-    async getAvailabilityForAcc() {
-      await AvailabilityDataService.getByUser(this.user.userId)
-        .then((response) => {
-          this.userAvailability = response.data;
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    },
-    async getAvailabilityForInst() {
-      await AvailabilityDataService.getByUser(this.user.userId)
-        .then((response) => {
-          this.userAvailability = response.data;
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    },
-    async retrieveEvents() {
-      await EventDataService.getAll()
-        .then((response) => {
-          this.events = response.data;
-          // console.log(this.events);
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-
-      // Narrow down to events in the future
-    },
-    async retrieveEventTimes(eventId) {
-      await EventTimeDataService.getByEvent(eventId)
-        .then((response) => {
-          this.currentEventTimes = response.data;
-          // console.log(this.currentEventTimes);
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-
-      // Narrow down to events in the future
     },
     formatTime(time) {
       return new Date("January 1, 2000 " + time).toLocaleTimeString("us-EN", {
@@ -579,50 +400,6 @@ export default {
         minute.toString().padStart(2, "0") +
         ":00"
       );
-    },
-    async determineEventTimes() {
-      this.selectedEventTimes = [];
-      await this.retrieveEventTimes(this.currentEvent.id);
-    },
-
-    async retrieveCourses() {
-      await CourseDataService.getAll()
-        .then((response) => {
-          this.courses = response.data;
-          this.filteredCourses = this.courses;
-          // console.log(this.courses);
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    },
-    changeCurrentEvent(event) {
-      this.errorMessage = "";
-      this.selectedEvent = event;
-      // this.chosenEvent();
-      this.currentSlot = this.userChosenSlot.filter(
-        (obj) => obj.eventId == event.id
-      );
-      this.showDialog = true;
-    },
-    chosenEvent() {
-      let tempTime = this.selectedEvent.startTime;
-
-      while (tempTime <= this.selectedEvent.endTime) {
-        // this.availabilitySlots.push({
-        //   title: this.formatTime(tempTime),
-        //   value: tempTime,
-        // });
-        tempTime = this.addDurationMinutes(tempTime);
-      }
-
-      this.availabilityEndArray = Array.from(this.availabilitySlots);
-      this.availabilityStartArray = Array.from(this.availabilitySlots);
-
-      this.availabilityEndArray.shift(); //removes the first timeslot from the end array
-      this.availabilityStartArray.pop(); //removes the last timeslot from the start array
-
-      this.availabilityStart = this.availabilityStartArray[0];
     },
   },
   setup() {
