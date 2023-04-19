19fe997537a13ffe570f7872d725b88edfc32c40
Andrew Clubb <andrew.clubb@eagles.oc.edu>
2023-04-19 10:36:36 -0500

    loading data into the table

--- src/components/faculty/FacultyCreateCritique.vue
+++ src/components/faculty/FacultyCreateCritique.vue
@@ -3,42 +3,52 @@
     <v-card-title> Faculty Create Critique: </v-card-title>
   </v-card>
   <v-container>
-    <v-row>
-      <v-spacer></v-spacer>
-      <strong>{{
-        "Today's " + this.timeslots.type + ": " + this.timeslots.name
-      }}</strong>
-      <v-spacer></v-spacer>
-    </v-row>
-    <v-data-table :headers="headers" :items="timeslots" class="elevation-1">
-      <!-- <template v-slot:top>
-        <v-toolbar flat>
-          <v-toolbar-title>UPCOMING EVENTS</v-toolbar-title>
-        </v-toolbar>
-      </template>
-      <template #item="{ item }">
-        <tr>
-          <td v-for="(header, index) in headers" :key="index">
-            <div
-              v-if="header.title == 'Start Time' || header.title == 'End Time'"
-            >
-              {{ this.formatTime(item.columns[header.key]) }}
-            </div>
-            <div v-else-if="header.title != 'Actions'">
-              {{ item.columns[header.key] }}
-            </div>
-            <div v-else>
-              <v-btn
-                small
-                color="primary"
-                @click="displayEventAvailability(item.raw)"
-                >Create Availability</v-btn
+    <div v-if="timeslots.length === 0">
+      <v-row>
+        <v-spacer></v-spacer>
+        <strong>There are no timeslots today</strong>
+        <v-spacer></v-spacer>
+      </v-row>
+    </div>
+    <div v-else>
+      <v-data-table
+        :headers="headers"
+        :items="timeslots.eventTimeslots"
+        class="elevation-1"
+      >
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-toolbar-title>{{
+              "Today's " + this.timeslots.type + ": " + this.timeslots.name
+            }}</v-toolbar-title>
+          </v-toolbar>
+        </template>
+        <template #item="{ item }">
+          <tr>
+            <td v-for="(header, index) in headers" :key="index">
+              <div
+                v-if="
+                  header.title == 'Start Time' || header.title == 'End Time'
+                "
               >
-            </div>
-          </td>
-        </tr>
-      </template> -->
-    </v-data-table>
+                {{ this.formatTime(item.columns[header.key]) }}
+              </div>
+              <div v-else-if="header.title != 'Actions'">
+                {{ item.columns[header.key] }}
+              </div>
+              <div v-else>
+                <v-btn
+                  small
+                  color="primary"
+                  @click="displayEventAvailability(item.raw)"
+                  >Create Availability</v-btn
+                >
+              </div>
+            </td>
+          </tr>
+        </template>
+      </v-data-table>
+    </div>
   </v-container>
 </template>
 <script>
@@ -47,24 +57,41 @@ export default {
   name: "FacultyCreateCritique",
   data: () => ({
     timeslots: [],
-    // headers: [
-    //   { title: "Event Type", key: "type" },
-    //   { title: "Event Date", key: "date" },
-    //   { title: "Start Time", key: "startTime" },
-    //   { title: "End Time", key: "endTime" },
-    //   { title: "Actions", key: "actions", sortable: false },
-    // ],
+    headers: [
+      { title: "Student(s)", key: "studentNames" },
+      { title: "Start Time", key: "startTime" },
+      { title: "End Time", key: "endTime" },
+      { title: "Actions", key: "actions", sortable: false },
+    ],
   }),
   methods: {
     async fillTimeslots() {
       await EventDataService.getStudentTimeslotsForEvent(this.eventId)
         .then((response) => {
           this.timeslots = response.data[0];
+          this.timeslots.eventTimeslots.forEach((obj) => {
+            var names = [];
+            obj.studentTimeslots.forEach((stuObj) => {
+              names.push(
+                stuObj.studentInstrument.student.user.fName +
+                  " " +
+                  stuObj.studentInstrument.student.user.lName
+              );
+            });
+
+            obj.studentNames = names.join(", ");
+          });
           console.log(this.timeslots);
         })
         .catch((err) => {
           console.log(err);
         });
+    },
+    formatTime(time) {
+      return new Date("January 1, 2000 " + time).toLocaleTimeString("us-EN", {
+        hour: "numeric",
+        minute: "numeric",
+      });
     },
   },
   async mounted() {
