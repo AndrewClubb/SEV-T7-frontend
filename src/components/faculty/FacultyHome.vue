9e884c84e7e2c39367636c2eb5d53eb999717873
Alexandria Adams <alexm.adams@yahoo.com>
2023-04-21 04:30:38 -0500

    Starting a new home

---
+++ src/components/faculty/FacultyHome.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card>
+    <v-card-title> Welcome Professor John Doe! </v-card-title>
+  </v-card>
+  <v-container>
+    <v-row>
+      <v-col cols="3">
+        <v-card>
+          <v-card-title align="center"> Your Students </v-card-title>
+        </v-card>
+      </v-col>
+      <v-col cols="9">
+        <v-card>
+          <v-card-title align="center"> Event Tasks: </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "facultyHome",
+  data: () => ({}),
+};
+</script>
