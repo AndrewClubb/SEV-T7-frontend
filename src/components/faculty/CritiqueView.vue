5277bb0e5bfb322e947034410c67e0fa9d626bf8
Andrew Clubb <andrew.clubb@eagles.oc.edu>
2023-04-26 13:09:04 -0500

    updating to use student

--- src/components/faculty/CritiqueView.vue
+++ src/components/faculty/CritiqueView.vue
@@ -2,7 +2,7 @@
   <v-card>
     <v-card-title> Event Critiques: </v-card-title>
   </v-card>
-  <v-container>
+  <!-- <v-container>
     <v-row>
       <v-col cols="3">
         <v-select
@@ -122,6 +122,129 @@
         <v-btn color="primary" @click="showDialog = false">Close</v-btn>
       </v-card-actions>
     </v-card>
+  </v-dialog> -->
+  <v-container>
+    <v-data-table
+      :sort-by="[{ key: 'lName', order: 'asc' }]"
+      :headers="studentHeaders"
+      :items="students"
+      :search="studentSearch"
+      class="elevation-1"
+    >
+      <template #top>
+        <v-toolbar flat>
+          <v-toolbar-title> STUDENTS </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="studentSearch"
+            append-icon="mdi-magnify"
+            label="Search by name or email"
+            single-line
+            hide-details
+            clearable
+            class="mr-2"
+          ></v-text-field>
+        </v-toolbar>
+      </template>
+      <template #item="{ item }">
+        <tr>
+          <td v-for="(header, index) in studentHeaders" :key="index">
+            <div v-if="header.title == 'Actions'">
+              <v-btn
+                size="small"
+                class="ml-2"
+                color="primary"
+                @click="displayStudent(item.raw)"
+              >
+                View Critiques
+              </v-btn>
+            </div>
+            <div v-else>
+              {{ item.columns[header.key] }}
+            </div>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
+  </v-container>
+  <v-dialog v-model="dialog" :style="{ width: '1000px' }" class="mx-auto">
+    <v-card>
+      <v-card-title></v-card-title>
+      <v-card-text>
+        <v-expansion-panels variant="accordion">
+          <v-expansion-panel v-for="event in events">
+            <v-expansion-panel-title>
+              <v-row>
+                <v-col>
+                  <v-row>
+                    <strong>Date:&nbsp;</strong>
+                    {{ event.date }}
+                  </v-row>
+                </v-col>
+                <v-col>
+                  <v-row>
+                    <strong>Name:&nbsp;</strong>
+                    {{ event.name }}
+                  </v-row>
+                </v-col>
+                <v-col>
+                  <v-row>
+                    <strong>Type:&nbsp;</strong>
+                    {{ " " + event.type }}
+                  </v-row>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-title>
+            <v-expansion-panel-text
+              v-for="eventTimeslot in event.eventTimeslots"
+            >
+              <v-card color="fav">
+                <v-card-title>
+                  {{
+                    eventTimeslot.studentTimeslots[0].studentInstrument
+                      .instrument.name
+                  }}
+                </v-card-title>
+                <v-card-text>
+                  <v-row>
+                    <v-col
+                      v-for="jurorTimeslot in eventTimeslot.jurorTimeslots"
+                      cols="4"
+                    >
+                      <v-card elevation="15">
+                        <v-card-title>
+                          {{
+                            jurorTimeslot.userRole.user.fName +
+                            " " +
+                            jurorTimeslot.userRole.user.lName
+                          }}
+                        </v-card-title>
+                        <v-row no-gutters>
+                          <v-col
+                            v-for="critique in jurorTimeslot.critiques"
+                            cols="6"
+                          >
+                            <v-card-text>
+                              <strong>{{ critique.type }}</strong>
+                              <div v-if="critique.grade">
+                                {{ "Grade: " + critique.grade }}
+                              </div>
+                              <div v-if="critique.comment">
+                                {{ "Comment: " + critique.comment }}
+                              </div>
+                            </v-card-text></v-col
+                          ></v-row
+                        >
+                      </v-card>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+              </v-card>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-card-text>
+    </v-card>
   </v-dialog>
 </template>
 <script>
@@ -132,23 +255,33 @@ export default {
   data: () => ({
     semesters: [],
     selectedSemester: null,
-    semesterCritiques: [],
-    filteredCritiques: [],
-    studentFilterArray: [],
-    studentFilter: null,
-    typeFilterArray: [],
-    typeFilter: null,
-    monthFilterArray: [],
-    monthFilter: null,
-    showDialog: false,
-    selectedStudent: null,
-    headers: [
-      { title: "Event Date", key: "eventTimeslot.event.date" },
-      { title: "First Name", key: "studentInstrument.student.user.fName" },
-      { title: "Last Name", key: "studentInstrument.student.user.lName" },
-      { title: "Event Type", key: "eventTimeslot.event.type" },
-      { title: " " },
+    // semesterCritiques: [],
+    // filteredCritiques: [],
+    // studentFilterArray: [],
+    // studentFilter: null,
+    // typeFilterArray: [],
+    // typeFilter: null,
+    // monthFilterArray: [],
+    // monthFilter: null,
+    // showDialog: false,
+    // selectedStudent: null,
+    // headers: [
+    //   { title: "Event Date", key: "eventTimeslot.event.date" },
+    //   { title: "First Name", key: "studentInstrument.student.user.fName" },
+    //   { title: "Last Name", key: "studentInstrument.student.user.lName" },
+    //   { title: "Event Type", key: "eventTimeslot.event.type" },
+    //   { title: " " },
+    // ],
+    students: [],
+    studentHeaders: [
+      { title: "First Name", key: "fName" },
+      { title: "Last Name", key: "lName" },
+      { title: "Email", key: "email" },
+      { title: "Actions", sortable: false, allign: "end" },
     ],
+    studentSearch: "",
+    dialog: false,
+    events: [],
   }),
   methods: {
     async retrieveAllSemesters() {
@@ -176,77 +309,101 @@ export default {
     async semesterSearchUpdate(semester) {
       await EventDataService.getSemesterCritiques(semester)
         .then((response) => {
-          this.semesterCritiques = response.data;
-          this.semesterCritiques.forEach((entry) => {
-            entry.stuName =
-              entry.studentInstrument.student.user.fName +
-              " " +
-              entry.studentInstrument.student.user.lName;
-
-            entry.month = new Date(
-              entry.eventTimeslot.event.date
-            ).toLocaleDateString("us-EN", { month: "long" });
-          });
-          this.filteredCritiques = this.semesterCritiques;
-          this.fillFilters();
+          this.students = response.data;
+          console.log(response.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    async displayStudent(student) {
+      console.log(this.selectedSemester.id, student.id);
+      await EventDataService.getStudentSemesterCritiques(
+        this.selectedSemester.id,
+        student.id
+      )
+        .then((response) => {
+          this.events = response.data;
         })
         .catch((e) => {
           console.log(e);
         });
+      this.dialog = true;
     },
-    displayStudentCritiques(student) {
-      this.selectedStudent = student;
-      this.showDialog = true;
-    },
-    fillFilters() {
-      let set = new Set();
-      this.studentFilter = undefined;
-      this.semesterCritiques.forEach((obj) => set.add(obj.stuName));
-      this.studentFilterArray = Array.from(set).sort();
+    // async semesterSearchUpdate(semester) {
+    //   await EventDataService.getSemesterCritiques(semester)
+    //     .then((response) => {
+    //       this.semesterCritiques = response.data;
+    //       this.semesterCritiques.forEach((entry) => {
+    //         entry.stuName =
+    //           entry.studentInstrument.student.user.fName +
+    //           " " +
+    //           entry.studentInstrument.student.user.lName;
 
-      set = new Set();
-      this.typeFilter = undefined;
-      this.semesterCritiques.forEach((obj) =>
-        set.add(obj.eventTimeslot.event.type)
-      );
-      this.typeFilterArray = Array.from(set);
+    //         entry.month = new Date(
+    //           entry.eventTimeslot.event.date
+    //         ).toLocaleDateString("us-EN", { month: "long" });
+    //       });
+    //       this.filteredCritiques = this.semesterCritiques;
+    //       this.fillFilters();
+    //     })
+    //     .catch((e) => {
+    //       console.log(e);
+    //     });
+    // },
+    // displayStudentCritiques(student) {
+    //   this.selectedStudent = student;
+    //   this.showDialog = true;
+    // },
+    // fillFilters() {
+    //   let set = new Set();
+    //   this.studentFilter = undefined;
+    //   this.semesterCritiques.forEach((obj) => set.add(obj.stuName));
+    //   this.studentFilterArray = Array.from(set).sort();
 
-      set = new Set();
-      this.monthFilter = undefined;
-      this.semesterCritiques.forEach((obj) => set.add(obj.month));
-      this.monthFilterArray = Array.from(set);
-    },
-    filterCritiques() {
-      if (
-        this.studentFilter != undefined ||
-        this.typeFilter != undefined ||
-        this.monthFilter != undefined
-      ) {
-        this.filteredCritiques = this.semesterCritiques.filter((obj) => {
-          var isValid = true;
+    //   set = new Set();
+    //   this.typeFilter = undefined;
+    //   this.semesterCritiques.forEach((obj) =>
+    //     set.add(obj.eventTimeslot.event.type)
+    //   );
+    //   this.typeFilterArray = Array.from(set);
 
-          if (this.studentFilter != undefined) {
-            isValid = obj.stuName == this.studentFilter;
-          }
-          if (isValid && this.typeFilter != undefined) {
-            isValid = obj.eventTimeslot.event.type == this.typeFilter;
-          }
-          if (isValid && this.monthFilter != undefined) {
-            isValid = obj.month == this.monthFilter;
-          }
+    //   set = new Set();
+    //   this.monthFilter = undefined;
+    //   this.semesterCritiques.forEach((obj) => set.add(obj.month));
+    //   this.monthFilterArray = Array.from(set);
+    // },
+    // filterCritiques() {
+    //   if (
+    //     this.studentFilter != undefined ||
+    //     this.typeFilter != undefined ||
+    //     this.monthFilter != undefined
+    //   ) {
+    //     this.filteredCritiques = this.semesterCritiques.filter((obj) => {
+    //       var isValid = true;
 
-          return isValid;
-        });
-      } else {
-        this.filteredCritiques = this.semesterCritiques;
-      }
-    },
-    formatDate(date) {
-      return new Date(date).toLocaleDateString("us-EN", {
-        month: "long",
-        day: "numeric",
-      });
-    },
+    //       if (this.studentFilter != undefined) {
+    //         isValid = obj.stuName == this.studentFilter;
+    //       }
+    //       if (isValid && this.typeFilter != undefined) {
+    //         isValid = obj.eventTimeslot.event.type == this.typeFilter;
+    //       }
+    //       if (isValid && this.monthFilter != undefined) {
+    //         isValid = obj.month == this.monthFilter;
+    //       }
+
+    //       return isValid;
+    //     });
+    //   } else {
+    //     this.filteredCritiques = this.semesterCritiques;
+    //   }
+    // },
+    // formatDate(date) {
+    //   return new Date(date).toLocaleDateString("us-EN", {
+    //     month: "long",
+    //     day: "numeric",
+    //   });
+    // },
   },
   async mounted() {
     await this.retrieveAllSemesters();
